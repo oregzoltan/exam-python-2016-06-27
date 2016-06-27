67d18a32df69fa9942e897ce3a2dfe8c9720b558
Oreg Zoltan <oreg_zoltan@yahoo.com>
2016-06-27 10:29:14 +0200

    add first.py

--- first.py
+++ first.py
@@ -3,3 +3,12 @@
 # It should raise an error if the parameter is not a list
 # example: [1, 2, 3, 4, 5] should produce [2, 4]
 
+def every_second_element(raw_list):
+    if type (raw_list) != list:
+        raise ValueError('Expected a list')
+    modified_list = []
+    for i in range(1,len(raw_list),2):
+        modified_list.append(raw_list[i])
+    return modified_list
+
+print(every_second_element([1, 2, 3, 4, 5]))
